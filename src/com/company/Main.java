c140f6cd1b20b107f13e88fd1a5dace52a6295a2
apontej22 <japonte@students.mchenry.edu>
2017-09-04 09:12:53 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        byte myByte = 3;
+        short mySh = 15;
+        int myInt = 39;
+        long myLn = 1054;
+        float myfl = 1.5f;
+        double myDouble = 15.253;
+        boolean myBoolean = true;
+        char mychar = 'j';
+
+
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + mySh);
+        System.out.println("Int: " + myInt);
+        System.out.println("Long: " + myLn);
+        System.out.println("Float: " + myfl);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Boolean: " + myBoolean);
+        System.out.println("Char: " + mychar);
+
+    }
+}
